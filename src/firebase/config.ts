9566c1b349f4d5bfe984ab5c43d0acabd23f66c1
卄ellS <thiagoalexandrecosta49@gmail.com>
2026-06-01 07:50:50 +0000

    restore firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6696860128-c7a6f",
-  "appId": "1:230285150149:web:ae474006c312252abf2dac",
-  "apiKey": "AIzaSyBJki1BY5pTFKVINyQpDXDJvRh9JBn1_t4",
-  "authDomain": "studio-6696860128-c7a6f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "230285150149",
-  "storageBucket": "studio-6696860128-c7a6f.firebasestorage.app"
+  projectId: "studio-6696860128-c7a6f",
+  appId: "1:230285150149:web:ae474006c312252abf2dac",
+  apiKey: "AIzaSyBJki1BY5pTFKVINyQpDXDJvRh9JBn1_t4",
+  authDomain: "studio-6696860128-c7a6f.firebaseapp.com",
+  messagingSenderId: "230285150149",
+  storageBucket: "studio-6696860128-c7a6f.firebasestorage.app"
 };
